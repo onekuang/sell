04c8598a6f1c9eba1650f085f6a8a960db061b32
unknown <>
2017-06-09 15:35:18 +0800

    0.2

---
+++ src/components/head.vue
@@ -0,0 +1,22 @@
+<template>
+	 <div class="header">
+	 	<div class="content-wrapper">
+	 		<div class="avatar"></div>
+	 		<div class="bulletin-wrapper"></div>
+	 	</div>
+	 </div>
+</template>
+
+<script>
+	export default {
+		props: {
+			seller: {
+				type: Object
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- src/components/header/head.vue
+++ src/components/header/head.vue
@@ -4,21 +4,192 @@
 	 		<div class="avatar">
 	 			<img :src="seller.avatar" width="64" height="65">
 	 		</div>
-	 	</div>
-	 	<div class="bulletin-wrapper"></div>
+	 	
+		 	<div class="content">
+				<!-- 头像 -->
+		 		<div class="title">
+		 			<span class="brand"></span>
+		 			<span class="name">{{seller.name}}</span>
+		 		</div>
+
+		 		<div class="description">
+		 			{{seller.description}}/{{seller.deliveryTime}}分钟到达
+		 		</div>
+				
+		 		<div v-if="seller.supports" class="support" >
+		 			<span class="icon" :class="classMap[seller.supports[0].type]"></span>
+		 			<span class="text">{{seller.supports[0].description}}</span>
+		 		</div>
+		 	</div>
+			<!-- 活动 -->
+		 	<div v-if="seller.supports" class="support-count" @click="showDetail">
+		 		<span class="count">{{seller.supports.length}}个</span>
+		 		<i class="count-icon">&nbsp;&gt;</i>
+		 	</div>
+		</div>
+		<!-- 公告 -->
+		<div class="bulletion-wrapper" @click="showDetail">
+			<span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+			<i class="count-icon">&nbsp;&gt;</i>
+		</div>
+		<!-- 模糊背景 -->
+		<div class="background">
+			<img :src="seller.avatar" width="100%" height="100%">
+		</div>
+
+		<div v-show="detailShow" class="detail" >
+			
+		</div>
 	 </div>
+
+
 </template>
 
 <script>
 export default {
   props: {
     seller: {
-    type: Object
+      type: Object
     }
+  },
+  data() {
+    return {
+      detailShow: false
+    }
+  },
+  methods: {
+    showDetail() {
+      this.detailShow = true
+    }
+  },
+  created() {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
   }
 }
 </script>
 
-<style scoped>
+<style lang="stylus" rel="stylesheet/stylus">
+	@import "../../common/stylus/mixin"
+	.header
+		position: relative;
+		overflow: hidden;
+		background: rgba(7,17,27,0.5);
+		color: #fff;
+		.content-wrapper
+			position: relative;
+			padding: 24px 12px 18px 24px
+			font-size: 0
+			.avatar
+				display: inline-block
+				vertical-align: top;
+				img
+					border-radius: 2px;
+			.content
+				display: inline-block
+				margin-left: 16px
+				font-size :14px
+				.title
+					margin: 2px 0 8px 0
+					.brand
+						display: inline-block;
+						vertical-align: top;
+						width: 30px
+						height: 18px
+						bg-image('brand')
+						background-size: 30px 18px
+						background-repeat: no-repeat
+					.name
+						margin-left: 6px;
+						font-size: 16px;
+						line-height: 18px;
+						font-weight: 900;
+				.description
+					margin-bottom: 10px;
+					line-height: 12px;
+					font-size: 12px;
+				.support
+					.icon
+						display: inline-block;
+						vertical-align: top;
+						width: 12px;
+						height: 12px;
+						margin-right: 4px;
+						background-size: 12px 12px;
+						background-repeat: no-repeat;
+						&.decrease
+							bg-image('decrease_1')
+						&.discount
+							bg-image('discount_1')
+						&.guarantee
+							bg-image('guarantee_1')
+						&.invoice
+							bg-image('invoice_1')
+						&.special
+							bg-image('special_1')
+					.text
+						vertical-align: top;
+						line-height: 12px;
+						font-size: 10px;
+			.support-count
+				position: absolute;
+				right: 12px;
+				bottom: 14px;
+				padding: 0 8px;
+				height: 24px;
+				line-height: 24px;
+				border-radius:14px
+				background: rgba(0,0,0,0.2);
+				text-align: center;
+				.count
+					font-size: 10px;
+				.count-icon
+					margin-left: 2px;
+					line-height: 24px;
+					font-size: 10px
+		/*公告*/
+		.bulletion-wrapper
+			position: relative;
+			height: 28px;
+			line-height: 28px;
+			padding: 0 22px 0 12px;
+			white-space: nowrap;
+			overflow: hidden;
+			text-overflow:ellipsis;
+			background: rgba(7,17,27,0.2)
 
+			.bulletin-title
+				display: inline-block;
+				vertical-align: top;
+				margin-top: 8px;
+				width: 22px;
+				height: 12px;
+				bg-image('bulletin')
+				background-size: 22px 12px;
+			.bulletin-text
+				vertical-align: top;
+				margin:0 4px;
+				font-size: 10px;
+			.count-icon
+				position: absolute;
+				font-size: 10px;
+				right: 12px;
+		/*模糊背景*/
+		.background
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			z-index: -1;
+			filter: blur(10px)
+		/*详情*/
+		.detail
+			position: fixed;
+			z-index: 100;
+			width: 100%;
+			height: 100%;
+			overflow: auto;
+			background: rgba(7,17,27,0.8);
+			top: 0;
+			left: 0;
 </style>
